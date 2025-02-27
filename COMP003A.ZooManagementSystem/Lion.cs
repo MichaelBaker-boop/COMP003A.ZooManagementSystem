4eb66a860c03118efedbeb5aa98d03f9e4ca8baf
Michael Baker <michaelbaker4@my.whccd.edu>
2025-02-26 23:04:41 -0800

    Added Lion Class

---
+++ COMP003A.ZooManagementSystem/Lion.cs
@@ -0,0 +1,23 @@
+﻿namespace COMP003A.ZooManagementSystem
+{
+    internal class Lion : Animal
+    {
+        /// <summary>
+        /// Constructs a lion with name and species.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="species"></param>
+        public Lion(string name, string species)
+        {
+            Name = name;
+            Species = species;
+        }
+        /// <summary>
+        /// Shows the sound a lion makes.
+        /// </summary>
+        public override void MakeSound()
+        {
+            Console.WriteLine("The lion roars!");
+        }
+    }
+}
