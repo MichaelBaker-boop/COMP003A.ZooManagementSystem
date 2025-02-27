e9bd7336ec7727a5a67bd45e543d2f7fb681fbce
Michael Baker <michaelbaker4@my.whccd.edu>
2025-02-26 23:15:24 -0800

    Added zoo utility

---
+++ COMP003A.ZooManagementSystem/ZooUtility.cs
@@ -0,0 +1,22 @@
+﻿namespace COMP003A.ZooManagementSystem
+{
+    internal class ZooUtility
+    {
+
+        public string DescribeAnimal(string name)
+        {
+            return name;
+        }
+
+        public string DescribeAnimal(string name, string species)
+        {
+            return name + species;
+        }
+
+        public string DescribeAnimal(string name, string species, int age)
+        {
+            return name + species + age;
+        }
+
+    }
+}
