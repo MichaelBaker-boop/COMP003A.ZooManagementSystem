498f9fc098c551df458191aedb179839644608e1
Michael Baker <michaelbaker4@my.whccd.edu>
2025-02-26 23:06:59 -0800

    Added Parrot class

---
+++ COMP003A.ZooManagementSystem/Parrot.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Linq;
+
+namespace COMP003A.ZooManagementSystem
+{
+    internal class Parrot : Animal
+    {
+        /// <summary>
+        /// Constructs a parrot with name and species.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="species"></param>
+        public Parrot(string name, string species)
+        {
+            Name = name;
+            Species = species;
+        }
+        /// <summary>
+        /// Shows the sound a parrot makes.
+        /// </summary>
+        public override void MakeSound()
+        {
+            Console.WriteLine("The parrot squawks!");
+        }
+    }
+}
