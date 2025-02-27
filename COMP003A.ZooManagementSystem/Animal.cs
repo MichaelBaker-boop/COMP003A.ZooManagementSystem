7e0e078ffa2a65dae7af9a937a39f6534a590b82
Michael Baker <michaelbaker4@my.whccd.edu>
2025-02-26 22:55:09 -0800

    Add project files.

---
+++ COMP003A.ZooManagementSystem/Animal.cs
@@ -0,0 +1,24 @@
+﻿namespace COMP003A.ZooManagementSystem
+{
+    public abstract class Animal
+    {
+        // Fields
+        private string _name;
+        private string _species;
+
+        // Properties
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Species
+        { 
+            get { return _species; } 
+            set { _species = value; } 
+        }
+
+        public abstract void MakeSound();
+    }
+}
