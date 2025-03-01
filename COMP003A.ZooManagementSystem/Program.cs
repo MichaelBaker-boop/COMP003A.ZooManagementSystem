ef295142cdeb0de78d9cbf8a2d719c0c489df730
Michael Baker <michaelbaker4@my.whccd.edu>
2025-02-28 21:57:13 -0800

    Replaced menu if ladder with switch menu

--- COMP003A.ZooManagementSystem/Program.cs
+++ COMP003A.ZooManagementSystem/Program.cs
@@ -4,6 +4,7 @@
 // Purpose: Zoo management system demonstrating inheritance, abstraction, polymorphism, and method overloading in C#
 using System.ComponentModel.Design;
 using System.Security.Cryptography.X509Certificates;
+using System.Xml.Serialization;
 
 namespace COMP003A.ZooManagementSystem
 {
@@ -28,67 +29,71 @@ namespace COMP003A.ZooManagementSystem
                 menuSelection = int.Parse(Console.ReadLine());
 
 
-                if (menuSelection == 1)
+                switch (menuSelection)
                 {
-                    bool validInput = false;
+                    case 1:
 
+                        bool validInput = false; // Initialize test of user input
 
-                    Animal myLion = new Lion("Simba", "Mountain");
-                    while (!validInput)
-                    {
+                        Animal myLion = new Lion("Simba", "Mountain"); // Create new Lion Object
 
-                        Console.WriteLine("Enter the name of the lion: ");
-                        myLion.Name = Console.ReadLine();
-                        if (string.IsNullOrEmpty(myLion.Name))
+                        while (!validInput) // While loop to get a name that is not null or empty
                         {
-                            Console.WriteLine("The input cannot be null or empty");
+                            Console.WriteLine("Enter the name of the lion: ");
+                            myLion.Name = Console.ReadLine();
+                            if (string.IsNullOrEmpty(myLion.Name))
+                            {
+                                Console.WriteLine("The input cannot be null or empty");
+                            }
+                            else
+                            {
+                                break;
+                            }
                         }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                    while (!validInput)
-                    {
-                        Console.WriteLine("Enter the species of the lion: ");
-                        myLion.Species = Console.ReadLine();
-                        if (string.IsNullOrEmpty(myLion.Species))
-                        {
-                            Console.WriteLine("The input cannot be null or empty");
-                        }
-                        else
-                        {
-                            validInput = true;
-                        }
-                    }
 
-                    Console.WriteLine($"{myLion.Name}, {myLion.Species}");
-                    animal.Add(myLion);
-                    Console.WriteLine("Lion added successfully");
+                        while (!validInput) // While loop to get a species that is not null or empty
+                        {
+                            Console.WriteLine("Enter the species of the lion: ");
+                            myLion.Species = Console.ReadLine();
+                            if (string.IsNullOrEmpty(myLion.Species))
+                            {
+                                Console.WriteLine("The input cannot be null or empty");
+                            }
+                            else
+                            {
+                                validInput = true;
+                            }
+                        }
 
-                }
-                
-        
-                
-                if (menuSelection == 2)
-                {
-                    Console.WriteLine("Option 2");
-                }
-                if (menuSelection == 3)
-                {
-                    for (int i = 0; i < animal.Count; i++)
-                    {
-                        Console.WriteLine($"{animal[i].Name}, {animal[i].Species}");
-                    }
-                }
-                if (menuSelection == 4)
-                {
-                    Console.WriteLine("Option 4");
-                }
-                if (menuSelection == 5)
-                {
-                    Console.WriteLine("Goodbye!");
-                    menu = false;
+                        Console.WriteLine($"{myLion.Name}, {myLion.Species}"); // Check inputs; remove or comment before submission
+                        animal.Add(myLion);
+                        Console.WriteLine("Lion added successfully");
+                        break;
+
+                    case 2:
+
+                        Console.WriteLine("Option 2");
+                        break;
+                    case 3:
+                    
+                        for (int i = 0; i < animal.Count; i++)
+                        {
+                            Console.WriteLine($"{animal[i].Name}, {animal[i].Species}");
+                        }
+                        break;
+                    case 4:   
+
+                        Console.WriteLine("Option 4");
+                        break;
+                    case 5:
+                    
+                        Console.WriteLine("Goodbye!");
+                        menu = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice, please select an available option.");
+                        break;
+                        
                 }
             }
 
